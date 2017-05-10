2c9692078766c9cac9312343d66f79ffa48ff139
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-10 17:30:54 +0200

    change order of methods in sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,12 +12,6 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   ends
 
-  # Logs out the current user.
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
-
   # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -26,5 +20,11 @@ module SessionsHelper
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+  end
+
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
