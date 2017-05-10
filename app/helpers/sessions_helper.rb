9c40caba7b15fc63c40b3c18739126fed8852843
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-10 17:29:31 +0200

    add remember user in session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+   # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  ends
+
   # Logs out the current user.
   def log_out
     session.delete(:user_id)
