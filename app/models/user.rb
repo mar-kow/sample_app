8d1bf4036bd92fcc3032b1ddd48ba35d9fa200d4
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-10 17:23:21 +0200

    add method authenticated?(remember_token) in user moderl

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,8 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+ # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
